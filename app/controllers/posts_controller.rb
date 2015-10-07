c5d734f65af6ed6695d00cb8d4bd13e28f815e77
Warren Kushner <warren.kushner@gmail.com>
2015-10-07 02:58:52 +0000

    added create update destroy ability for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,54 @@
 class PostsController < ApplicationController
+    
+    before_action :find_post, only: [:update,:edit,:destroy,:show]
+    
      def index
+         @posts = Post.all
      end
+     
+     def new
+         @post = Post.new
+     end
+     
+     def create
+         @post = Post.new(post_params)
+         if @post.save
+             redirect_to @post
+         else
+             render 'new'
+         end
+     end
+     
+     def update
+         if @post.update(post_params)
+             redirect_to @post
+         else
+             render 'edit'
+         end
+     end
+     
+     def edit
+     end
+     
+     def destroy
+         if @post.destroy
+             redirect_to root_path
+         else
+             render 'edit'
+         end
+     end
+     
+     def show
+     end
+     
+     private
+     
+     def find_post
+         @post=Post.find(params[:id])
+     end
+     
+     def post_params
+         params.require(:post).permit(:title,:link,:description)
+     end
+     
 end
